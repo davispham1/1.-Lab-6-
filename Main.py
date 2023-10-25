f3276a0eba2f327e3e9c83df4342424f2e0a15b4
davispham1 <davispham1126@gmail.com>
2023-10-25 17:27:26 -0400

    menu

---
+++ Main.py
@@ -0,0 +1,22 @@
+
+'''def encoder(password):
+    for i in range(len(password))'''
+
+
+while True:
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit\n')
+    option = input('Please enter an option: ')
+    if option == '1':
+        password = input('Please enter your password to encode: ')
+        encoded_password = # encode(password)
+        print('Your password has been encoded and stored!\n')
+    elif option == '2':
+
+
+
+
+
